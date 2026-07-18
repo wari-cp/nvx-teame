e4776fd10d0325a6e0c214752878c9eee41da1b3
wari-cp <ofixa54@gmail.com>
2026-07-18 04:00:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
+import {
+  getAuth,
+  GithubAuthProvider,
+  signInWithPopup
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+const firebaseConfig = {
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  ...
+};
