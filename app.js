1655166eca3df8d493852c3c1e71f22d818312cb
wari-cp <ofixa54@gmail.com>
2026-07-18 04:04:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ import {
   signInWithPopup
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  ...
+  apiKey: "AIzaSyBQl35dej2wrTxct02i27xf4vekzXMExw8",
+  authDomain: "nvx-team.firebaseapp.com",
+  projectId: "nvx-team",
+  storageBucket: "nvx-team.firebasestorage.app",
+  messagingSenderId: "471144466289",
+  appId: "1:471144466289:web:ae919909ac18fb56dcd8d0"
 };
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const provider = new GithubAuthProvider();
